1a420cdb6a75099378e7afc6ddc03818b22e20af
ccastrom <claudiocastrom7@gmail.com>
2024-07-16 20:00:10 -0400

    The kerosene values are obtained in the console according to the previously assigned search criteria

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ const  scrapper= async()=>{
     await page.select('#comuna','06101')
    
     await page.click('#sidebar  form  div.d-grid.gap-2  button.btn.btn-primary');
+    await page.waitForSelector('#infinite-list');
+    const kerosenePriceList=await page.evaluate(()=>{
+        const keroseneValue=document.querySelector('#infinite-list div div div div.col-md-2.precio.side-content').innerText;
+        return keroseneValue;
+       
+    });
+    console.log("The value is: "+kerosenePriceList);
 }
 
 scrapper();

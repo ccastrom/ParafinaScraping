a3f595cb2e347dace4d8eadaadeb51819958f622
ccastrom <claudiocastrom7@gmail.com>
2024-07-18 21:59:40 -0400

    Getting desires values:kerosene,ubication & location. Showed in console

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
 import puppeteer from 'puppeteer';
 
-const  scrapper= async()=>{
+const scrapper = async () => {
 
-    const openBrowser=await puppeteer.launch({
-        headless:false,
-        slowMo:200,
-       
-});
-  const context = openBrowser.defaultBrowserContext();
-  await context.overridePermissions('https://www.parafinaenlinea.cl', ['geolocation']);
+    const openBrowser = await puppeteer.launch({
+        headless: false,
+        slowMo: 200,
 
-    const page=await openBrowser.newPage();
+    });
+    const context = openBrowser.defaultBrowserContext();
+    await context.overridePermissions('https://www.parafinaenlinea.cl', ['geolocation']);
+
+    const page = await openBrowser.newPage();
     await page.setViewport({ width: 1250, height: 1250 })
     await page.goto('https://www.parafinaenlinea.cl');
-   
-    await page.select('#region','06')
-    await page.select('#comuna','06101')
-   
+
+    await page.select('#region', '06')
+    await page.select('#comuna', '06101')
+
     await page.click('#sidebar  form  div.d-grid.gap-2  button.btn.btn-primary');
     await page.waitForSelector('#infinite-list');
-    const kerosenePriceList=await page.evaluate(()=>{
-        const keroseneValue=document.querySelector('#infinite-list div div div div.col-md-2.precio.side-content').innerText;
-        return keroseneValue;
-       
+    const kerosenePriceList = await page.evaluate(() => {
+        const keroseneValue = document.querySelectorAll(".card")
+        return Array.from(keroseneValue).map(document=>{
+            const priceWebPageClassElement=document.querySelector('.precio');
+            const ubicationWebPageClassElement=document.querySelectorAll('.card-subtitle');
+
+            const price = priceWebPageClassElement ? priceWebPageClassElement.innerText.trim() : 'No disponible';
+
+            const getUbicationValues = Array.from(ubicationWebPageClassElement).map(ubicationElement => ubicationElement.innerText.trim());
+            const region = getUbicationValues[0] || 'No disponible';
+            const location = getUbicationValues[1] || 'No disponible';
+
+             
+           
+           
+
+            return { price, region,location };
+        })
+      
+
+
+
     });
-    console.log("The value is: "+kerosenePriceList);
+    console.log(kerosenePriceList);
 }
 
 scrapper();
